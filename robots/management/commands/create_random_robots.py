f680173aebed66d2dee1bd5debf86f3d38310947
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-17 18:50:26 +0500

    Create django command create_random_robots

---
+++ robots/management/commands/create_random_robots.py
@@ -0,0 +1,23 @@
+from django.core.management import BaseCommand
+
+from robots.factories import RobotFactory
+
+
+class Command(BaseCommand):
+    help = "Create random robots."
+
+    def add_arguments(self, parser):
+        parser.add_argument("count", type=int, help="Count of robots")
+
+    def handle(self, *args, **kwargs):
+        count = kwargs["count"]
+        self.stdout.write(f"Creating {count} random robots...")
+
+        for num in range(count):
+            robot = RobotFactory.create()
+            self.stdout.write(
+                f"Create #{num} robot:\n"
+                f"pk={robot.pk} serial={robot.pk} "
+                f"model={robot.model} version={robot.version} "
+                f"created={robot.created}\n"
+            )
