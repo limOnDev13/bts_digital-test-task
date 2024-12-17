d6dedbd88a34ea0ce9501f4c18a1600d987248fd
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-17 16:46:22 +0500

    Add docstrings

--- robots/business/xlsx.py
+++ robots/business/xlsx.py
@@ -1,4 +1,3 @@
-from tempfile import NamedTemporaryFile
 from typing import List, Set
 
 from openpyxl import Workbook
@@ -7,21 +6,25 @@ from .models import ProducedRobots, ProducedRobotsSummary
 
 
 class ProducedRobotsXLSX(ProducedRobotsSummary):
+    """A class for saving the summary in the format .xlsx."""
+
     def __init__(self, summary: List[ProducedRobots]):
         super().__init__(summary)
         self.wb = Workbook()
 
     def create_sheets_with_headers(self):
+        """Create pages for each model and record column headings."""
         models: Set[str] = {robot.model for robot in self.produced_robots}
         for model in models:
             ws = self.wb.create_sheet(model)
             ws.append(ProducedRobots.fields())
 
     def write_in_workbook(self):
+        """Write data to the table."""
         for robot in self.produced_robots:
             self.wb[robot.model].append(robot.data_in_list)
 
-    def save_file(self, tmp: NamedTemporaryFile) -> None:
+    def save_file(self, tmp) -> None:
         self.create_sheets_with_headers()
         self.write_in_workbook()
         self.wb.save(tmp.name)
