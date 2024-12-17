10035e46ef4049ce273a08ce87b335c1fa016909
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-17 16:45:57 +0500

    Create module models.py; create dataclass ProducedRobots; create abstract class ProducedRobotsSummary

---
+++ robots/business/models.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any, List, Tuple
+
+
+@dataclass
+class ProducedRobots:
+    """Class for storing information about produced robots."""
+
+    model: str
+    version: str
+    num_robots: int
+
+    @classmethod
+    def fields(cls) -> Tuple[str, str, str]:
+        """Get fields in the class."""
+        return "model", "version", "num_robots"
+
+    @property
+    def data_in_list(self) -> List[Any]:
+        """Get data in list."""
+        return [getattr(self, field) for field in self.fields()]
+
+
+class ProducedRobotsSummary(ABC):
+    """
+    An abstract class for saving a summary to a temporary named file.
+
+    Args:
+        summary (List[ProducedRobots]): a summary of the robots produced
+         that needs to be saved.
+    """
+
+    def __init__(self, summary: List[ProducedRobots]):
+        self.produced_robots = summary
+
+    @abstractmethod
+    def save_file(self, tmp) -> None:
+        """
+        Save the summary to a temporary file.
+
+        :param tmp: an open temporary file object (using tempfile.NamedTemporaryFile).
+        :return: None
+        """
+        pass
