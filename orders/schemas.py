862bf2fc6f48ee4d7e7651bef27b1ad7812e7a74
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-19 13:09:52 +0500

    Create module schemas.py for pydantic schemas; create schema OrderSchema for validating json with info about a new order

---
+++ orders/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field, field_validator
+
+from customers.models import Customer
+
+
+class OrderSchema(BaseModel):
+    customer_id: int
+    robot_serial: str = Field(max_length=5)
+
+    @field_validator("customer_id")
+    @classmethod
+    def validate_customer_in_db(cls, value: int):
+        """Validate that this customer is in the database."""
+        if not Customer.objects.filter(pk=value).exists():
+            raise ValueError(f"Customer with id {value} does not exists.")
+        return value
