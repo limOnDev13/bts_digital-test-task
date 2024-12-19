2a1e5774fb5e5dde8c33466354bc80bd1096feaf
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-19 13:08:56 +0500

    Create module tests.py for testing the orders business module; create test case CreateNewOrderTestCase for testing func create_new_order; add tests test_create_new_order, test_create_new_order_with_large_number_of_robots, test_create_new_order_without_robots

---
+++ orders/business/tests.py
@@ -0,0 +1,88 @@
+import random
+from typing import List
+
+from django.conf import settings
+from django.test import TestCase
+
+from business.queue import CustomersQueue
+from customers.models import Customer
+from orders.models import Order
+
+from .main import create_new_order
+from .models import OrderInfo
+
+
+class CreateNewOrderTestCase(TestCase):
+
+    def setUp(self):
+        self.serial = "R2-D2"
+        self.customer = Customer.objects.create(email=settings.TEST_EMAIL)
+        self.order_info = OrderInfo(
+            customer_id=self.customer.pk, robot_serial=self.serial
+        )
+        self.queue = CustomersQueue()
+
+    def tearDown(self):
+        self.customer.delete()
+        self.queue.clear()
+
+    def test_create_new_order(self):
+        num_robots: int = random.randint(0, 10)
+        num_orders: int = random.randint(1, 10)
+
+        for _ in range(num_robots):
+            self.queue.increase_num(self.serial)
+
+        orders: List[Order] = list()
+        for i in range(num_orders):
+            order, _ = create_new_order(self.order_info)
+
+            self.assertTrue(Order.objects.filter(pk=order.pk).exists())
+
+        if num_orders <= num_robots:
+            self.assertEqual(
+                self.queue.num_serials(self.serial), num_robots - num_orders
+            )
+            self.assertEqual(self.queue.len_queue(self.serial), 0)
+        else:
+            self.assertEqual(self.queue.num_serials(self.serial), 0)
+            self.assertEqual(self.queue.len_queue(self.serial), num_orders - num_robots)
+
+        for order in orders:
+            order.delete()
+
+    def test_create_new_order_with_large_number_of_robots(self):
+        num_robots: int = random.randint(3, 10)
+        num_orders: int = random.randint(1, num_robots)
+
+        for _ in range(num_robots):
+            self.queue.increase_num(self.serial)
+
+        orders: List[Order] = list()
+        for i in range(num_orders):
+            order, in_stock = create_new_order(self.order_info)
+
+            self.assertTrue(Order.objects.filter(pk=order.pk).exists())
+            self.assertTrue(in_stock)
+            self.assertEqual(self.queue.num_serials(self.serial), num_robots - i - 1)
+            self.assertEqual(self.queue.len_queue(self.serial), 0)
+
+            orders.append(order)
+
+        for order in orders:
+            order.delete()
+
+    def test_create_new_order_without_robots(self):
+        num_orders: int = random.randint(1, 10)
+
+        orders: List[Order] = list()
+        for i in range(num_orders):
+            order, in_stock = create_new_order(self.order_info)
+
+            self.assertTrue(Order.objects.filter(pk=order.pk).exists())
+            self.assertFalse(in_stock)
+            self.assertEqual(self.queue.num_serials(self.serial), 0)
+            self.assertEqual(self.queue.len_queue(self.serial), i + 1)
+
+        for order in orders:
+            order.delete()
